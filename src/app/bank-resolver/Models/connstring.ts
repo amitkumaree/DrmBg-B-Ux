18ee474e42dca03149d9db8e7875776eab70f094
surajit999 <54792786+surajit999@users.noreply.github.com>
2021-03-06 23:23:19 +0530

    update

--- src/app/bank-resolver/Models/connstring.ts
+++ src/app/bank-resolver/Models/connstring.ts
@@ -2,7 +2,7 @@ export class connstring {
   public server: string;
   public userId: string;
   public password: string;
-  public siguserId: string;
-  public sigpassword: string;
+  public sigUserId: string;
+  public sigPassword: string;
 }
 
