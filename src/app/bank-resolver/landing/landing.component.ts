f8adfcf97e9435736e1637f02c2587d5e09c225e
abhisekmandal003 <31164926+abhisekmandal003@users.noreply.github.com>
2021-06-24 17:05:18 +0530

    dashboard

--- src/app/bank-resolver/landing/landing.component.ts
+++ src/app/bank-resolver/landing/landing.component.ts
@@ -1,5 +1,9 @@
 import { InAppMessageService } from './../../_service/in-app-message.service';
 import { Component, OnInit } from '@angular/core';
+import { mm_dashboard } from '../Models/mm_dashboard';
+import { p_gen_param } from '../Models/p_gen_param';
+import { SystemValues } from '../Models';
+import { RestService } from 'src/app/_service';
 
 @Component({
   selector: 'app-landing',
@@ -8,11 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor(private msg: InAppMessageService) { }
-
+  constructor(private msg: InAppMessageService,private svc: RestService) { }
+  sys = new SystemValues();
+  dashboardItem = new mm_dashboard();
+  isLoading = false;
   ngOnInit(): void {
     // when ever landing is loaded screen title should be hidden
     this.msg.sendhideTitleOnHeader(true);
+    this.getDashboardItem();
   }
 
-}
+  getDashboardItem() {
+    debugger;
+    this.isLoading = true;
+    const param = new p_gen_param();
+    param.brn_cd=this.sys.BranchCode;
+    this.svc.addUpdDel<any>('Common/GetDashBoardInfo', param).subscribe(
+        res => {
+          debugger;
+          this.isLoading = false;
+          this.dashboardItem = res;
+        },
+        err => {
+          this.isLoading = false;
+          ;
+        }
+      );
+    }
+   
+  }
+
--- src/app/bank-resolver/system/daycomplition/daycomplition.component.ts
+++ src/app/bank-resolver/system/daycomplition/daycomplition.component.ts
@@ -51,6 +51,7 @@ checkComplete()
 }
 private dayCompletionCall (clsDt :any,clsamt:any)
 {
+  debugger;
   this.showMsg =null;
   var pgp = new p_gen_param();
   pgp.brn_cd = this.sys.BranchCode;
@@ -60,7 +61,7 @@ private dayCompletionCall (clsDt :any,clsamt:any)
   ;
   this.svc.addUpdDel<any>('Sys/W_DAY_CLOSE', pgp).subscribe(
     res => {
-      ;
+      debugger;
       this.isLoading = false;
       this.alertMsg = res.output;
       this.closingdata.setValue['closingbal']=0;
