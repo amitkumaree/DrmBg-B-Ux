da67df502d5b8aa51319301a8c335edf01cea754
abhisekmandal003 <31164926+abhisekmandal003@users.noreply.github.com>
2021-09-21 07:59:47 +0530

    kk

--- src/app/bank-resolver/UCIC/utself-help/utself-help.component.ts
+++ src/app/bank-resolver/UCIC/utself-help/utself-help.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { RestService } from 'src/app/_service';
+import { ShowMessage, SystemValues } from '../../Models';
 
 @Component({
   selector: 'app-utself-help',
@@ -7,9 +12,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UTSelfHelpComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private svc: RestService, private frmBldr: FormBuilder,private modalService: BsModalService,
+    private router: Router) { }
+  get f() { return this.shgFrm.controls; }
+  shgFrm: FormGroup;
+  branchCode = '0';
+  userName = '';
+  sys = new SystemValues();
+  isLoading = false;
+  showMsg: ShowMessage;
   ngOnInit(): void {
+    this.branchCode = this.sys.BranchCode;
+    this.userName = this.sys.UserId;
+    this.shgFrm = this.frmBldr.group({
+     shg_id : [], 
+     brn_cd : [],   
+     chairman_name : [],        
+     secretary_name : [],       
+     village : [],              
+     gruop_sex : [],            
+      monthly_subcription : [], 
+      min_member_limit : [],    
+      male_member : [],         
+      female_member : [],       
+      caste_sc : [],            
+      caste_st : [],            
+      caste_gen : [],           
+      caste_muslim : [],        
+     form_dt : [],            
+     sb_accno : []            
+    });
+    this.onShgMemberCreation = this.formBuilder.group({
+      'ShgMemberF': this.formBuilder.array([
+        this.addshgMemberFromGroup()
+      ])
+    });
   }
+
+  addshgMemberFromGroup(): FormGroup 
+  return this.formBuilder.group({
+   shg_id : [null, null],           
+   shg_member_id : [null, null],    
+   shg_member_name : [null, null],   
+   guardian_name : [null, null],     
+   shg_member_sex : [null, null],    
+   shg_member_caste : [null, null],  
+   religion : [null, null],	         
+   date_of_join : [null, null],     
+   education : [null, null],         
+   brn_cd : [null, null],            
+   status : [null, null],            
+   date_of_birth : [null, null],    
+   age : [null, null],              
+   widow : [null, null],             
+   toilet_flag : [null, null],       
+   mobile : [null, null],           
+   adhar_no : [null, null],         
+   pan : [null, null],               
+   disability_remarks : [null, null],
+   training_remarks : [null, null]
+    });
+
+  retrieveData(){}
+  clearData(){}
+  saveData(){}
+  deleteData(){}
+  backScreen(){this.router.navigate([this.sys.BankName + '/la']);}
 
 }
